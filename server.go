19b6025c1d4a4f57abeb6745219ad976e18abf8f
CrazyCollin <lazycollinl@gmail.com>
2022-05-06 22:35:54 +0800

    update todo:http bugfix

--- server.go
+++ server.go
@@ -8,6 +8,7 @@ import (
 	"io"
 	"log"
 	"net"
+	"net/http"
 	"reflect"
 	"strings"
 	"sync"
@@ -290,6 +291,43 @@ func (server *Server) handleRequest(cc codec.Codec, req *request, sending *sync.
 	case <-called:
 		<-sent
 	}
+}
+
+const (
+	connected        = "200 Connected to Go RPC"
+	defaultRPCPath   = "/_gorpc_"
+	defaultDebugPath = "/debug/gorpc"
+)
+
+//
+// ServeHTTP
+// @Description: 处理HTTP请求
+// @receiver server
+// @param w
+// @param req
+//
+func (server *Server) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	if req.Method != "CONNECT" {
+		w.Header().Set("Content-Type", "text/plain;charset=utf-8")
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		_, _ = io.WriteString(w, "405 must connect\n")
+		return
+	}
+	conn, _, err := w.(http.Hijacker).Hijack()
+	if err != nil {
+		log.Print("rpc hijacking ", req.RemoteAddr, ": ", err.Error())
+		return
+	}
+	_, _ = io.WriteString(conn, "HTTP/1.0 "+connected+"\n\n")
+	server.ServeConn(conn)
+}
+
+// HandleHTTP is a convenient approach for default server to register HTTP handlers
+func (server *Server) HandleHTTP() {
+	DefaultServer.HandleHTTP()
+	http.Handle(defaultRPCPath, server)
+	http.Handle(defaultDebugPath, debugHTTP{server})
+	log.Println("rpc server debug path:", defaultDebugPath)
 }
 
 func Accept(lis net.Listener) {
